ce5291f2e7b9338c3e32e62fa58eb5f91e9ca752
Denis Toledo <contato@denistoledo.com.br>
2017-04-22 11:48:53 -0300

    slideshow home

--- dev/js/main.js
+++ dev/js/main.js
@@ -52,13 +52,29 @@ var Pousada = {
   slideshow: function() {
     'use strict';
 
-    $('#slideshow').vegas({
+    $('#slideshow-home').vegas({
       slides: [
-        { src: './assets/images/suites/Brilhante/Foto-suite-36-brilhante-perspectiva-01.jpg' },
-        { src: './assets/images/suites/Brilhante/Foto-suite-36-brilhante-perspectiva-02.jpg' }
+        { src: './assets/images/suites/Brilhante/Foto-suíte-36-brilhante-perspectiva-02.jpg' },
+        { src: './assets/images/suites/Brilhante/Foto-suíte-36-brilhante-perspectiva-04.jpg' },
+        { src: './assets/images/suites/Rubi-Duplex/Suíte-17/foto-suíte-17-rubi-duplex-perspectiva-01.jpg' },
+        { src: './assets/images/suites/Rubi/Suíte-35/foto-suíte-35-rubiperspectiva-04.jpg' },
+        { src: './assets/images/suites/Esmeralda/Suíte-24/Foto-suíte-24-esmeralda-perspectiva-01.jpg' },
+        { src: './assets/images/suites/Perola/Suíte-20/foto-suíte-20-perola-vintage-perspectiva-01.jpg' },
+        { src: './assets/images/suites/Diamante/Suíte-27/foto-suite-27-diamante-perspectiva-01.JPG' },
+        { src: './assets/images/suites/Ouro/Suíte-33/Foto-suíte-33-ouro-perspectiva-01.jpg' },
+        { src: './assets/images/suites/Prata/foto-suíte-08-prata-perspectiva-01.jpg' }
       ],
-      timer: true,
-      overlay: './assets/images/overlay.png'
+      timer: true
+    });
+
+    $('#slideshow-suites').vegas({
+      slides: [
+        { src: './assets/images/Empresa-Interior-e-Lavanderia/Foto-faxada-perspectiva.jpg' },
+        { src: './assets/images/suites/Perola/Suíte-20/foto-suíte-20-perola-vintage-perspectiva-01.jpg' },
+        { src: './assets/images/suites/Brilhante/Foto-suite-36-brilhante-perspectiva-02.jpg' },
+        { src: './assets/images/suites/Diamante/Suíte-27/foto-suite-27-diamante-perspectiva-01.JPG' }
+      ],
+      timer: false
     });
   }
 }
